146f470a90896b6568cb82796a22cd57473cbfb0
cathjime <xcatherine.jimenez@gmail.com>
2020-09-08 16:48:32 -0400

    comments-controller-show-index-actions

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,2 +1,12 @@
 class Api::V1::CommentsController < ApplicationController
+    def index
+        comments = Comment.all
+        render json: comments
+      end
+      
+      def show
+        commment = Comment.find(params:[id])
+        render json: comment
+      end
+
 end
