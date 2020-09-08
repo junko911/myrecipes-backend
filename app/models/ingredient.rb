05baf544625fe4d81dd923162c52fcd8c6dc2447
junko911 <junkotahara911@gmail.com>
2020-09-08 17:07:04 -0400

    Set dairy_free? method

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,12 @@
 class Ingredient < ApplicationRecord
     has_many :ingredient_recipes
     has_many :recipes, through: :ingredient_recipes
+
+    def dairy_free?
+        dairy_products = ['milk', 'yogurt', 'cream', 'butter', 'cheese', 'casein', 'custard', 'ice cream']
+        dairy_products.each do |dairy|
+            return true if name.include?(dairy) 
+        end
+        false
+    end
 end
