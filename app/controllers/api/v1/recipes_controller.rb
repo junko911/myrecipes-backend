880b9015107cfd0fc12d8eff3e4ee22b293f6aca
cathjime <xcatherine.jimenez@gmail.com>
2020-09-10 16:21:13 -0400

    changes to recipe controller

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::RecipesController < ApplicationController
+    def show
+        recipe = Recipe.find(params[:id])
+        render json: recipe
+      end
+      def update
+        recipe = Recipe.find(params[:id])
+        byebug
+        recipe.update(recipe_params)
+        render json: recipe
+      end
+      private
+      def recipe_params
+        params.require(:recipe).permit(:likes)
+      end
 end
